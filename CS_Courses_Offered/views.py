beca3fc763b064c54a1967d8230690b9091ce2d8
UltraRunner <dearultrarunners@gmail.com>
2016-02-26 00:41:03 -0700

    Json_Data

--- CS_Courses_Offered/views.py
+++ CS_Courses_Offered/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from .models import *
 from django.template import RequestContext
 from django.core import serializers
+import json
 
 # Create your views here.
 def index(request):
@@ -12,30 +13,29 @@ def index(request):
 def graph_data(request):
 	# get all data from offering model
 	# class_offered = Offering.objects.raw("SELECT * from Semester")
-	class_offered = Offering.objects.all()
+	class_offered = Offering.objects.filter(course_type = 'Lecture')
+
+	list = []
+	for row in class_offered:
+		list.append({'Act_enrol':row.act_enrollment,'Semester':row.a_semester.term_description, 'Class':row.a_course.course_title})
+	recipe_list_json = json.dumps(list)
+
 	class_offered1 = Offering.objects.filter(course_type = 'Lecture')
 	semester_offered = Semester.objects.order_by('id')
 	total_classes = Offering.objects.filter(course_type = 'Lecture')
-	json_data = serializers.serialize("json", Offering.objects.all())
-
-	# for obj in serializers.deserialize("json", json_data):
- #    do_something_with(obj)
-	# results = Semester.objects.include(field1__in=inner_qs)
-	# create a dictionary of allthe data in class_offered
+	json_data = serializers.serialize("json", Offering.objects.filter(course_type = 'Lecture'))
 
 	offered = {
-		"semester_classes" : class_offered,
 		"semester_classes1" : class_offered1,
-		"semesters": semester_offered,
-		"classes_type": total_classes,
-		"json_data": json_data
+		"json_data": json_data,
+		"serialized": recipe_list_json
 	}
 	# print(semester_offered)
 	# return JsonResponse(context_instance=RequestContext(request), 'graph/graphs.html', data, safe=False)
 	# return render_to_response('graph/graphs.html', semsters, context_instance=RequestContext(request))
 	return render_to_response('graph/graphs.html', offered, context_instance=RequestContext(request))
-	
-#this where I will tell what will be displayed. check out https://docs.djangoproject.com/en/1.8/intro/tutorial03/
+	# return HttpResponse(recipe_list_json, 'graph/graphs.html') #this just downloads the data
+# this where I will tell what will be displayed. check out https://docs.djangoproject.com/en/1.8/intro/tutorial03/
 #view loads the data using ORM and calls templates
 
 
